3b657ee0c5687af22204c14494028895af155c1d
Navaneet09 <navaneetmani@gmail.com>
2025-12-06 00:18:57 +0530

    feat: Add product details component with product fetching and add to cart functionality.

--- src/app/components/product-details/product-details.ts
+++ src/app/components/product-details/product-details.ts
@@ -11,38 +11,35 @@ import { CartItem } from '../../common/cart-item';
   selector: 'app-product-details',
   imports: [CurrencyPipe, RouterLink],
   templateUrl: './product-details.html',
-  styleUrl: './product-details.css'
+  styleUrl: './product-details.css',
 })
-
 export class ProductDetails {
-
   product!: Product;
 
-  constructor(private productService: ProductService, private route: ActivatedRoute, private cartService: cartService) { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private cartService: cartService
+  ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
       this.handleProductDetails();
     });
-  }  
+  }
 
   handleProductDetails() {
-    
     //get the "id" param string. convert string to a number using the "+" symbol
     const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
 
-    this.productService.getProduct(theProductId).subscribe(
-      data => {
-        this.product = data;
-      }
-    )
+    this.productService.getProduct(theProductId).subscribe((data) => {
+      this.product = data;
+    });
   }
 
   addToCart() {
-  
     console.log(`Adding to Cart: ${this.product.name}, ${this.product.unitPrice}`);
     const theCartItem = new CartItem(this.product);
     this.cartService.addToCart(theCartItem);
   }
-
 }
